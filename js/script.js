993a38166155393b1728eafbba99a5e411fb4f8c
*allan9595* <bohanzhang@my.unt.edu>
2019-06-09 20:37:26 -0400

    meet basic requirment, adding pagination allow page to be views in mutiple pages, improve userexperience

--- js/script.js
+++ js/script.js
@@ -18,6 +18,65 @@ FSJS project 2 - List Filter and Pagination
 ***/
 
 
+const listItems = document.getElementsByClassName("student-item cf");
+
+const pageItems = 10; 
+
+const showPage = (list, page) => {
+   const startIndex = (page * pageItems) - pageItems; //find out startIndex
+   const endIndex = page * pageItems; //find out where to end in a page
+   for(let i=0;i<list.length;i++){
+      if(i>=startIndex && i< endIndex){
+         list[i].style.display = "block" //if in range, display it with style.display property
+      } else {
+         list[i].style.display = "none" // if not range, not display it 
+      }
+   }  
+}
+
+//showPage(listItems, 1);
+
+const appendPageLinks = (list) => {
+   
+   const pagesNumber = Math.ceil(list.length / pageItems); //get the pages number needed round up to the upper number
+   const pagination = document.createElement("div"); //create a div tag
+   pagination.className = "pagination"; //assign the class to pagination
+
+   const page = document.querySelector(".page"); //get the page class
+   page.appendChild(pagination); //append newly created pagination to page
+
+   const ul = document.createElement("ul"); //create ul tag
+   pagination.appendChild(ul); //append ul to the div
+
+   
+   //create li, a, and ul and append them under div
+   for(let i=0;i<pagesNumber;i++){
+      let li = document.createElement("li"); //create a li tag
+      let a = document.createElement("a"); //create an a tag
+      ul.appendChild(li); //append created li tag to the parent ul tag
+      li.appendChild(a); //append created a tag to the parent li tag
+      a.setAttribute("href", "#"); //set tag a attribute as href = "#", means the link where take to the page top
+      a.appendChild(document.createTextNode(i+1)); //create text for the pagination, the text start at 1 instead of 0
+   }
+
+   document.querySelectorAll('a')[0].className = "active";
+
+   a1 = document.querySelectorAll('a');
+   showPage(list, 1); //default page is page 1 and display first 10 people
+   //loop through the page and change it based on cases
+   for(let x=0;x<a1.length;x++){
+      a1[x].addEventListener('click', (e) => {
+         showPage(list, x+1);
+         for(let y=0;y<a1.length;y++){
+            document.querySelectorAll("a")[y].className = ""; //the inner loop remove all the active class on every a tag
+         }
+         e.target.className = "active";
+      })
+      
+   }
+}
+
+appendPageLinks(listItems);
 
 
 /*** 
